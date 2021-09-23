8bce049461f437dcdfa4835c5889704cd45dfabc
Aklilu <aklilutsige@gmail.com>
2021-09-23 17:28:46 +0200

    Added unit test for user controller

---
+++ fourall/src/test/java/org/cmp/fourall/controllers/UserControllerUnitTest.java
@@ -0,0 +1,28 @@
+package org.cmp.fourall.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerUnitTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUserByID() {
+    }
+
+    @Test
+    void addNewUser() {
+    }
+
+    @Test
+    void updateExistUser() {
+    }
+
+    @Test
+    void deleteExistUser() {
+    }
+}
